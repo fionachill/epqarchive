32822f6139989c8c30e2ad8659c7678e634b5277
Fiona Hill <fionacchill@gmail.com>
2025-04-01 12:59:06 +0100

    UI adjusted for registration page and implementation of react-hook-form begun

--- epq-react-app/src/pages/registrationPage.tsx
+++ epq-react-app/src/pages/registrationPage.tsx
@@ -3,48 +3,82 @@ import Header from "../components/header";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
+import { useForm } from "react-hook-form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 const RegistrationPage: React.FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [reenterPassword, setReenterPassword] = useState("");
 
-    const [valid, setValid] = useState(false);
+    const {
+            register, 
+            handleSubmit,
+            formState: {errors}, 
+        } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(`Submitted: `, data);
+    };
+
+    useEffect(() => {
+        
+    })
 
 
-    
-
-return (
+    return (
         <>
             <Header />
             <Container fluid>
                 <Row>
-                    <Col>
-                        <h3>Register</h3>
-                    </Col>
+                    <Col><br></br></Col>
                 </Row>
                 <Row className="justify-content-md-center">
                     <Col cs lg="4">
-                        <Form>
-                            <Form.Group className="mb-3" controlId="email">
-                                <Form.Label>Email Address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="password">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="reenterpassword">
-                                <Form.Label>Re-enter Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            <Button variant="info" type="submit" disabled={!valid}>
-                                Register
-                            </Button>
-                        </Form>
+                        <Card>
+                            <Form onSubmit={handleSubmit(onSubmit)}>
+                                <Form.Group className="mb-3" controlId="email">
+                                    <Form.Label>Email Address</Form.Label>
+                                    <Form.Control
+                                        required
+                                        type="email" 
+                                        placeholder="Enter email address"
+                                        {...register("email")} 
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        Please provide a valid email address
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control 
+                                        required
+                                        type="password" 
+                                        placeholder="Password"
+                                        {...register("password")}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        Please provide a password
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="reenterpassword">
+                                    <Form.Label>Re-enter Password</Form.Label>
+                                    <Form.Control
+                                        required
+                                        type="password" 
+                                        placeholder="Password" 
+                                        {...register("reenterpassword")}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        Please ensure passwords match
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Button variant="info" type="submit">
+                                    Register
+                                </Button>
+                            </Form>
+                        </Card> 
                     </Col>
                 </Row>
             </Container>
